1fcb268e038bc66c51e8b6a2a6df457c662cfc22
yi.liu <yi.liu@chebian.help>
2017-05-04 17:43:06 +0800

    change project

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import dva from 'dva';
-import './index.css';
+import './index.html'
+import 'babel-polyfill'
+import dva from 'dva'
+import createLoading from 'dva-loading'
+import { browserHistory } from 'dva/router'
+import { message } from 'antd'
 
 // 1. Initialize
 const app = dva({
-  initialState: {
-    products: [
-      { name: 'dva', id: 1 },
-      { name: 'antd', id: 2 },
-    ],
+  ...createLoading(),
+  history: browserHistory,
+  onError (error) {
+    message.error(error.message)
   },
-});
+})
 
-// 2. Plugins
-// app.use({});
+// 2. Model
+app.model(require('./models/app'))
 
-// 3. Model
-// app.model(require('./models/example'));
-app.model(require('./models/products'));
+// 3. Router
+app.router(require('./router'))
 
-// 4. Router
-app.router(require('./router'));
-
-// 5. Start
-app.start('#root');
+// 4. Start
+app.start('#root')
